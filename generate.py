6903b7026ffcc57b418f1bb83027016d9f95fb7e
soft1q <36323486+soft1q@users.noreply.github.com>
2018-05-05 20:48:55 +0300

    Added context manager and generator

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
-# Второй модуль проекта "Генератор текста"
-# Генерация последовательности слов по модели
-# Версия: 2.0
-# Создано: Левашов Артём, МФТИ ФИВТ 790, 2018
+'''
+ Второй модуль проекта "Генератор текста"
+ Генерация последовательности слов по модели
+ Версия: 2.0
+ Создано: Левашов Артём, МФТИ ФИВТ 790, 2018
+'''
 
 # coding: utf-8
 import json
@@ -41,33 +43,30 @@ def choose_first_word(seed, freq_model):
         return seed
 
 
-def output_file(output):
-    """Определяет файл вывода или stdout"""
-    if output is None:
-        return sys.stdout
-    else:
-        try:
-            return open(output, 'w')
-        except OSError:
-            print("Не удалось открыть файл!")
+class make_output:
+    def __init__(self, file_name, method='w'):
+        if file_name is None:
+            self.file = sys.stdout
+        else:
+            self.file = open(file_name, method)
 
+    def __enter__(self):
+        return self.file
 
-def close_output(file, output):
-    """Закрывает файл, если вывод не в консоль"""
-    if output is not None:
-        file.close()
+    def __exit__(self, *args):
+        if self.file != sys.stdout:
+            self.file.close()
 
 
 def generate_sequence(prev_word, model, output, length):
     """Создание и вывод сгенерированной последовстельности"""
     output.write(prev_word + ' ')
     for i in range(1, length):
-        choice_list = list()
-        if len(model[prev_word].items()) != 0:
-            for word in model[prev_word].keys():
-                choice_list.extend([word] * model[prev_word][word])
-        else:
+        if len(model[prev_word].items()) == 0:
             break
+        choice_list = list()
+        for word in model[prev_word].keys():
+            choice_list.extend([word] * model[prev_word][word])
         prev_word = random.choice(choice_list)
         output.write(prev_word + ' ')
 
@@ -81,8 +80,7 @@ if __name__ == "__main__":
     firstWord = choose_first_word(namespace.seed, model)
     n = namespace.length
     # Фиксируем файл вывода
-    f = output_file(namespace.output)
-    # Генерирование текста с помощью выбора из всех последователей
-    #   слова умноженных на частоту посредством random.choice
-    generate_sequence(firstWord, model, f, namespace.length)
-    close_output(f, namespace.output)
+    with make_output(namespace.output, 'w') as f:
+        # Генерирование текста с помощью выбора из всех последователей
+        #   слова умноженных на частоту посредством random.choice
+        generate_sequence(firstWord, model, f, namespace.length)
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
-# Первый модуль проекта "Генератор текста"
-# Обучение модели на заданных текстах
-# Версия: 2.0
-# Создано: Левашов Артём, МФТИ ФИВТ 790, 2018
+'''
+ Первый модуль проекта "Генератор текста"
+ Обучение модели на заданных текстах
+ Версия: 2.0
+ Создано: Левашов Артём, МФТИ ФИВТ 790, 2018
+'''
 
 # coding: utf-8
 import re
@@ -10,6 +12,8 @@ import argparse
 import sys
 import os
 from collections import defaultdict
+from collections import Counter
+from contextlib import contextmanager
 
 
 def input_parser():
@@ -26,37 +30,52 @@ def input_parser():
     return parser
 
 
-def push_bigrams_from_file(result_dict, input_file, lc):
+def count_bigrams(input_dir, lc):
     """Извлечь биграммы из файла в модель"""
-    last_word = "_START_"
-    for line in input_file:
-        tmp = re.findall(r'[a-zA-Zа-яА-Я]+', line)
-        for i, word in enumerate(tmp):
+    bigrams_counter = Counter()
+    for input_file in get_input(input_dir):
+        last_word = "_START_"
+        for line in input_file:
             if lc:
-                result_dict[last_word.lower()][word.lower()] += 1
-            else:
-                result_dict[last_word][word] += 1
-            last_word = word
-    result_dict[last_word] = {}
+                line = line.lower()
+            tmp = re.findall(r'[a-zA-Zа-яА-Я]+', line)
+            bigrams_counter[last_word, tmp[0]] += 1
+            last_word = tmp[-1]
+            for word1, word2 in zip(tmp[:-1], tmp[1:]):
+                bigrams_counter[word1, word2] += 1
+        bigrams_counter[last_word, "_END_"] += 1
+    return bigrams_counter
 
 
-def create_model(result_dict, input_dir, lc):
-    """Создание модели"""
+def get_input(input_dir):
     if input_dir is None:
-        push_bigrams_from_file(result_dict, sys.stdin, lc)
-    else:
-        for path, dirs, files in os.walk(input_dir):
-            for filename in files:
-                f = open(os.path.join(path, filename), 'r')
-                push_bigrams_from_file(result_dict, f, lc)
+        yield sys.stdin
+    for path, dirs, files in os.walk(input_dir):
+        for file in files:
+            f = open(os.path.join(path, file), 'r')
+            yield f
+            f.close()
+
+
+def create_model(bigrams_counter):
+    """Создание модели в виде словаря"""
+    result_dict = defaultdict(lambda: defaultdict(int))
+    for (word1, word2), frequency in bigrams_counter.items():
+        if word1 != "_START_":
+            if word2 == "_END_" and len(result_dict[word1].keys()) == 0:
+                result_dict[word1] = {}
+            else:
+                result_dict[word1][word2] = frequency
+    return result_dict
 
 
 if __name__ == "__main__":
     # Парсинг аргументов консольной команды
     namespace = input_parser().parse_args(sys.argv[1:])
-    # Словарь словарей, хранящий частоты пар
-    pairs_freq = defaultdict(lambda: defaultdict(int))
-    create_model(pairs_freq, namespace.input_dir, namespace.lc)
+    # Создание счётчика биграмм
+    bigrams = count_bigrams(namespace.input_dir, namespace.lc)
+    # Перевод счётчика биграмм в словарь словарей
+    pairs_freq = create_model(bigrams)
     # Загрузка модели в формате JSON
     with open(namespace.model, 'w') as f:
         json.dump(pairs_freq, f, ensure_ascii=False)
